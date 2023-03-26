2ee99952d8d55e3c957a38cd177ca39a3c29ca38
Nick Dreckshage <nicholasdreckshage@gmail.com>
2023-03-26 11:37:06 -0500

    testing

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,21 +1,53 @@
 import { Suspense } from 'react'
-import Header from './_components/header'
-import Content from './_components/content'
+// import Header from './_components/header'
+// import Content from './_components/content'
 
-// export const config = { runtime: 'edge' }
-export const revalidate = 0
+export const config = { runtime: 'edge' }
+// export const revalidate = 0
 
-export default function Web() {
+async function Product({ data }: { data: Promise<Response> }) {
+  const product = await data.then((res) => res.json())
+
+  return (
+    <p>
+      {product.id} - {product.name}
+    </p>
+  )
+}
+
+export default async function Web() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline text-purple-700">Web</h1>
-      <Suspense fallback={<p>Loading Header...</p>}>
+      {/* @ts-expect-error Async Server Component */}
+      <Product
+        data={fetch(`https://app-dir.vercel.app/api/products?id=1&delay=500`, {
+          cache: 'no-store',
+        })}
+      />
+      <Suspense fallback={<p>Loading Product 2...</p>}>
         {/* @ts-expect-error Async Server Component */}
-        <Header />
+        <Product
+          data={fetch(
+            `https://app-dir.vercel.app/api/products?id=2&delay=1500`,
+            {
+              cache: 'no-store',
+            }
+          )}
+        />
+        {/* <Header /> */}
       </Suspense>
-      <Suspense fallback={<p>Loading Content...</p>}>
+      <Suspense fallback={<p>Loading Product 3...</p>}>
         {/* @ts-expect-error Async Server Component */}
-        <Content />
+        <Product
+          data={fetch(
+            `https://app-dir.vercel.app/api/products?id=3&delay=2500`,
+            {
+              cache: 'no-store',
+            }
+          )}
+        />
+        {/* <Content /> */}
       </Suspense>
     </div>
   )
