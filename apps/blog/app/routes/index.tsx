489a14ad559d2cbc5f253686458d173790ad190a
Nick Dreckshage <nicholasdreckshage@gmail.com>
2023-03-26 19:55:12 -0500

    add blog

---
+++ apps/blog/app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div className="container">
+      <h1 className="title">Blog</h1>
+      <p className="description">Built With Turborepo & Remix</p>
+    </div>
+  )
+}
