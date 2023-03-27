086241f87427629280a0cd2d2218100a3b602549
Nick Dreckshage <nicholasdreckshage@gmail.com>
2023-03-26 20:20:40 -0500

    update

--- apps/blog/app/routes/index.tsx
+++ apps/blog/app/routes/index.tsx
@@ -1,8 +1,45 @@
-export default function Index() {
+import { Suspense } from 'react'
+import { defer } from '@vercel/remix'
+import { Await, useLoaderData } from '@remix-run/react'
+
+export const config = { runtime: 'edge' }
+
+export async function loader({ request }: LoaderArgs) {
+  return defer({
+    product1: await fetch(
+      `https://app-dir.vercel.app/api/products?id=1&delay=400`,
+      {
+        cache: 'no-store',
+      }
+    ).then((r) => r.json()),
+    product2: fetch(`https://app-dir.vercel.app/api/products?id=2&delay=1400`, {
+      cache: 'no-store',
+    }).then((r) => r.json()),
+    product3: fetch(`https://app-dir.vercel.app/api/products?id=3&delay=2400`, {
+      cache: 'no-store',
+    }).then((r) => r.json()),
+  })
+}
+
+const Product = ({ id, name }: { id: number; name: string }) => (
+  <p>
+    {id} - {name}
+  </p>
+)
+
+export default function Blog() {
+  const { product1, product2, product3 } = useLoaderData<typeof loader>()
+
   return (
-    <div className="container">
-      <h1 className="title">Blog</h1>
-      <p className="description">Built With Turborepo & Remix</p>
+    <div>
+      <h1 className="text-3xl font-bold underline text-purple-700">Blog</h1>
+      <Product {...product1} />
+      <Suspense fallback={<p>Loading Product 2 (1s)...</p>}>
+        <Await resolve={product2}>{(p) => <Product {...p} />}</Await>
+      </Suspense>
+      <Suspense fallback={<p>Loading Product 3 (2s)...</p>}>
+        <Await resolve={product3}>{(p) => <Product {...p} />}</Await>
+      </Suspense>
     </div>
   )
 }
