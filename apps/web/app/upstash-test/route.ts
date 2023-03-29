8b4509c3f078cd6a86a42fa9f62774123479eb13
Nick Dreckshage <nicholasdreckshage@gmail.com>
2023-03-28 23:31:32 -0500

    update

--- apps/web/app/upstash-test/route.ts
+++ apps/web/app/upstash-test/route.ts
@@ -1,11 +1,18 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const data = await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/get/foo`, {
-    headers: {
-      Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`,
-    },
-  }).then((response) => response.json())
+  const response = await fetch(
+    `${process.env.UPSTASH_REDIS_REST_URL}/get/foo`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`,
+      },
+    }
+  )
+
+  console.log(response.headers)
+
+  const data = await response.json()
 
   return NextResponse.json({ data })
 }
